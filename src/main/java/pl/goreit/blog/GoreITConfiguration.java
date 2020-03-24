ef678f98778e17019170c23a150234756d5f8b27
szczepkowskip <p.szczepkowski87@gmail.com>
2020-03-24 14:24:26 +0100

    add liquibase - init
    add hibernate
    
    add batch_product_import table in postgres

--- src/main/java/pl/goreit/blog/GoreITConfiguration.java
+++ src/main/java/pl/goreit/blog/GoreITConfiguration.java
@@ -1,5 +1,6 @@
 package pl.goreit.blog;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ConversionServiceFactoryBean;
@@ -16,6 +17,11 @@ public class GoreITConfiguration {
     public ConversionService goreITConversionService(ConversionServiceFactoryBean conversionServiceFactoryBean, Set<Converter<?, ?>> converters) {
         conversionServiceFactoryBean.setConverters(converters);
         return conversionServiceFactoryBean.getObject();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
     }
 
     @Bean
--- src/main/java/pl/goreit/blog/domain/facade/ProductFacade.java
+++ src/main/java/pl/goreit/blog/domain/facade/ProductFacade.java
@@ -1,14 +1,19 @@
 package pl.goreit.blog.domain.facade;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import pl.goreit.api.generated.product_api.ImportedProduct;
 import pl.goreit.blog.domain.model.Product;
+import pl.goreit.blog.domain.model.postgres.BatchProductImport;
 import pl.goreit.blog.domain.provider.ProductProvider;
 import pl.goreit.blog.infrastructure.mongo.ProductRepo;
+import pl.goreit.blog.infrastructure.postgres.BatchProductImportRepo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,13 +29,24 @@ public class ProductFacade {
     @Autowired
     private ConversionService goreITConversionService;
 
+    @Autowired
+    private BatchProductImportRepo batchProductImportRepo;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Scheduled(fixedRate = 10000)
-    public void importProducts() {
+    public void importProducts() throws JsonProcessingException {
         List<ImportedProduct> importedProducts = productProvider.provideProducts();
+
+        String productsJson = objectMapper.writeValueAsString(importedProducts);
+        batchProductImportRepo.save( new BatchProductImport(productsJson, LocalDateTime.now()));
+
         List<Product> products = importedProducts.stream()
                 .map(importedProduct -> goreITConversionService.convert(importedProduct, Product.class))
                 .collect(Collectors.toList());
 
         productRepo.saveAll(products);
+
     }
 }
